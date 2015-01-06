8265cf0fdfccfc9babf7c95f7ab48849377b891b
Yee W <yeekiwai@gmail.com>
2015-01-05 23:10:02 -0600

    Add config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,25 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Paperclip stuff
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
+  # Mailer
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address          => "smtp.gmail.com",
+    :port             => "587",
+    :domain           => "domain.com",
+    :authentication   => :plain,
+    :user_name        => ENV['EMAIL_USER'],
+    :password         => ENV['EMAIL_PASS']
+  }
 end
